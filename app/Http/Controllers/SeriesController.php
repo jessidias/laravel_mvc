d7c8538832f9c75bf60620236f839a46f1c26551
Jéssica Dias <jessib.dias@gmail.com>
2024-02-07 13:49:01 -0300

    Implementação MVC Laravel: criando uma aplicação com MVC

---
+++ app/Http/Controllers/SeriesController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use App\Models\Serie;
+
+
+class SeriesController extends Controller
+{
+    
+    public function index(){
+
+        $series = Serie::query()->orderBy('nome')->get();
+        //dd($series); // para debugar
+
+        return view('series.index')->with('series', $series);
+
+    }
+
+    public function create(){
+
+        return view('series.create');
+
+    }
+
+    public function store(Request $request){
+        $nomeSerie = $request->input('nome');
+
+        $serie = new Serie();
+        $serie-> nome = $nomeSerie;
+        $serie->save();
+        return redirect('/series');
+        
+
+    }
+}
